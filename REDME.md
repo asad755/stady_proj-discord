56d78c2fc87168027766ec6ebb71a1d039e5968b
asad755 <143116178+asad755@users.noreply.github.com>
2024-03-03 02:39:15 +0500

    stady with discord group

---
+++ REDME.md
@@ -0,0 +1,1 @@
+# IT proj with discord 
---
+++ main.ts
@@ -0,0 +1,19 @@
+// 3.	Name Cases: Store a person’s name in a variable, 
+// and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let person_name:string = "Muhammad ASSADIFTEKHAR";
+console.log(person_name.toLowerCase());
+console.log(person_name.toUpperCase());
+//  No inbuild method in JavaScript to titlecase the string. so we need to create the function to
+//  do the job.
+// Steps to create a function
+    // 1- convert the string to lowerCase.
+    // 2- split the single string into an array of strings.
+    // 3- create an array
+    // 4- loop through the array of strings and capitalize the first letter in every string.
+    // 5- return the title case string
+function toTitleCase(str: string){
+    let lower = str.toLowerCase().split(' ');
+    let char_array = [];
+    
+}
